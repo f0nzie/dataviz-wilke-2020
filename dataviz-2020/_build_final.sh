1ff30d7912a7450cb96d5a5199cd1115e3b978f1
f0nzie <alfonso.reyes@oilgainsanalytics.com>
2020-07-11 21:06:51 -0500

    shell scripts

---
+++ dataviz-2020/_build_final.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -rf ./_book_final/
+Rscript -e "bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::gitbook', config_file = '_bookdown_final.yml')"
+
+# copy the book to the shared folder in the host
+cp -r _book_final/ ../share/
