35edfc4958f84989b651353515c12551e7e35217
f0nzie <alfonso.reyes@oilgainsanalytics.com>
2020-07-12 16:31:05 -0500

    script to build the book

--- dataviz-2020/_build_final.sh
+++ dataviz-2020/_build_final.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 rm -rf ./_book_final/
+rm -rf ./_bookdown_files/
 Rscript -e "bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::gitbook', config_file = '_bookdown_final.yml')"
 
 # copy the book to the shared folder in the host
